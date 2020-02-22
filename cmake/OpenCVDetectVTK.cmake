1a5bc64b981bb2d2945f6499a3adbad12aad54bf
Adam Rankin <adam.rankin@gmail.com>
2020-02-22 14:03:05 -0500

    Enabling detection of VTK for any configuration type

--- cmake/OpenCVDetectVTK.cmake
+++ cmake/OpenCVDetectVTK.cmake
@@ -52,13 +52,19 @@ if(HAVE_QT AND VTK_VERSION VERSION_GREATER "6.0.0" AND NOT ${VTK_QT_VERSION} STR
   endif()
 endif()
 
-try_compile(VTK_COMPILE_STATUS
-    "${OpenCV_BINARY_DIR}"
-    "${OpenCV_SOURCE_DIR}/cmake/checks/vtk_test.cpp"
-    CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:STRING=${VTK_INCLUDE_DIRS}"
-    LINK_LIBRARIES ${VTK_LIBRARIES}
-    OUTPUT_VARIABLE OUTPUT
-)
+foreach(_config ${CMAKE_CONFIGURATION_TYPES})
+  set(CMAKE_TRY_COMPILE_CONFIGURATION ${_config})
+  try_compile(VTK_COMPILE_STATUS
+      "${OpenCV_BINARY_DIR}"
+      "${OpenCV_SOURCE_DIR}/cmake/checks/vtk_test.cpp"
+      CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:STRING=${VTK_INCLUDE_DIRS}"
+      LINK_LIBRARIES ${VTK_LIBRARIES}
+      OUTPUT_VARIABLE OUTPUT
+  )
+  if(${VTK_COMPILE_STATUS})
+    break()
+  endif()
+endforeach()
 
 if(NOT ${VTK_COMPILE_STATUS})
   message(STATUS "VTK support is disabled. Compilation of the sample code has failed.")
